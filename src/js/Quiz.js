7095a1efaed2c0292fc1b0d07b6ac0a509904028
mc222sn <mc222sn@student.lnu.se>
2018-12-05 18:02:09 +0100

    Changing module code

--- src/js/Quiz.js
+++ src/js/Quiz.js
@@ -2,13 +2,15 @@ import * as Time from './Time.js'
 
 const template = document.createElement('template')
 template.innerHTML = `
-<div id="question">
-  <h1 id="question-id" class="text-center""></h1>
-  <p id="question-text" class="text-center text-large""></p>
-</div>
+<form id="nickname" class="form-group">
+  <label class="form-label" for="input-example-1">Nickname</label>
+  <input class="form-input" name="nickname" type="text" placeholder="Nickname">
+  <input type="submit">
+</form>
 
-<form>
-  <input name="nickname" type="text">
+<form id="question" class="form-group">
+  <label class="form-label" for="input-example-1">Answer</label>
+  <input class="form-input" name="answer" type="text" placeholder="Answer">
   <input type="submit">
 </form>
 `
@@ -18,38 +20,53 @@ export class Quiz extends window.HTMLElement {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
-    this._questionDiv = this.shadowRoot.querySelector('#question')
+    this._questionForm = this.shadowRoot.querySelector('#question')
+    this._nicknameForm = this.shadowRoot.querySelector('#nickname')
     this._config = {
-      startUrl: 'http://vhost3.lnu.se:20080/question'
+      questionUrl: 'http://vhost3.lnu.se:20080/question',
+      answerUrl: 'http://vhost3.lnu.se:20080/answer'
     }
     this._nickname = ''
   }
 
   connectedCallback () {
-    this.shadowRoot.addEventListener('submit', this._submitNickname)
-    this.shadowRoot.addEventListener('submit', (event) => {
-      return this.getQuestions(1)
+    this._nicknameForm.addEventListener('submit', (event) => {
+      event.preventDefault() // removing /q=blabla in the url
+      this.nickname = event.target.nickname.value
+      event.target.hidden = true
+    })
+    this._questionForm.addEventListener('submit', (event) => {
+      event.preventDefault()
+      return this.getQuestion(1)
     })
   }
 
   disconnectedCallback () {
-    this.shadowRoot.removeEventListener('submit', this._submitNickname)
+    // lägg till alla här sen
   }
 
-  _submitNickname (event) {
-    event.preventDefault() // removing /q=blabla in the url
-    this.nickname = event.target.nickname.value
-    event.target.hidden = true
+  async getQuestion (id) {
+    let questionResult = await window.fetch(`${this._config.questionUrl}/${id}`)
+    return questionResult.json()
+    // console.log(qResults.json())
+    // this.sendAnswer(1, 2) // 1 is id and 2 the answer of the question id 1
+    // we send the answer here
   }
 
-  async getQuestions (id) {
-    let qResults = await window.fetch(`${this._config.startUrl}/${id}`)
-    // return qResults.json()
-    console.log(qResults.json())
-  }
-
-  async sendAnswerToQuestion (id) {
-
+  async sendAnswer (id, answer) {
+    let postReq = await window.fetch(`${this._config.answerUrl}/${id}`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        answer: answer, // sending serializing answer with post request
+      })
+    }).then( (response) => { 
+      console.log('woohoo!')
+      // we should add the new question id url to questionUrl here
+   })
   }
   
 }
