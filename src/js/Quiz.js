f61536aa5b535b7deacfce36a4bf6164c52bdcba
mc222sn <mc222sn@student.lnu.se>
2018-12-05 09:21:05 +0100

    Removing unessessary Player object

--- src/js/Quiz.js
+++ src/js/Quiz.js
@@ -1,28 +1,39 @@
-import * as Player from './Player.js'
 import * as Time from './Time.js'
 
-const questionTemplate = document.createElement('template')
-questionTemplate.innerHTML = `
-<h1 id="question-id" class="text-center"">Question #1</h1>
-<p id="question-text" class="text-center text-large"">
-Question here...
-</p>
+const template = document.createElement('template')
+template.innerHTML = `
+<h1 id="question-id" class="text-center""></h1>
+<p id="question-text" class="text-center text-large""></p>
+<form id="quiz">
+  <input name="nickname" type="text">
+  <input type="submit">
+</form>
 `
 
 export class Quiz extends window.HTMLElement {
   constructor () {
     super()
-    
     this.attachShadow({ mode: 'open' })
-    this.shadowRoot.appendChild(questionTemplate.content.cloneNode(true))
-    this._h1 = this.shadowRoot.querySelector('#question-id')
-    this._p = this.shadowRoot.querySelector('#question-text')
-    this._config = {
-      startUrl: 'http://vhost3.lnu.se:20080/question/1'
-    }
-    this._questions = {} // JSON data
-    this._players = {} // player objects 
-    this._highScores = {}
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this._tagName = this.shadowRoot.querySelectorAll('quiz-form')
+    this._form = this._tagName[0]
+    
+    this._input = null
+    // this._config = {
+    //   startUrl: 'http://vhost3.lnu.se:20080/question/1'
+    // }
+    // this._questions = {} // JSON data
+    this._nickname = ''
+    // this._highScores = {}
+  }
+
+  connectedCallback () {
+    this.shadowRoot.addEventListener('submit', this._submitNickname)
+  }
+
+  _submitNickname (event) {
+    event.preventDefault() // removing /q=blabla in the url
+    this.nickname = event.target.nickname.value
   }
   
   // getQuestions () {}
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 import * as Quiz from './Quiz.js'
 import * as Player from './Player.js'
 
-let player = document.createElement('player-form')
-// let quiz = document.createElement('quiz-form')
+// let player = document.createElement('player-form')
+let quiz = document.createElement('quiz-form')
 
-// document.querySelector('#quiz-container').appendChild(quiz)
-document.querySelector('#quiz-container').appendChild(player)
+document.querySelector('#quiz-container').appendChild(quiz)
+// document.querySelector('#quiz-container').appendChild(player)
