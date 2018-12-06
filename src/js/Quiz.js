2faef0295f160adb7ea109095e1ef40a76db945a
mc222sn <mc222sn@student.lnu.se>
2018-12-07 00:12:49 +0100

    Changing module code

--- src/js/Quiz.js
+++ src/js/Quiz.js
@@ -2,30 +2,11 @@ import * as Time from './Time.js'
 
 const template = document.createElement('template')
 template.innerHTML = `
-<form id="nickname" class="form-group">
-  <label class="form-label" for="input-example-1">Nickname</label>
-  <input class="form-input" name="nickname" type="text" placeholder="Nickname">
-  <input type="submit">
-</form>
-
 <form id="question" class="form-group">
-  <h3></h3>
-  <label class="form-label" for="input-example-1">Answer</label>
-  <input class="form-input" name="answer" type="text" placeholder="Answer">
-  
-  <br/>
-  <br/>
-  
-  <label>Choice</label>
-  <label>
-    <input type="radio" name="alt1" checked>
-    <span>Alternative 1</span>
-  </label>
-  
-  <br/>
-  <br/>
-
-  <input type="submit">
+  <h3 name="title"></h3>
+  <label>Answer</label>
+  <input name="answer" type="text" placeholder="Answer">
+  <input type="submit" value="Svara">
 </form>
 `
 
@@ -34,76 +15,39 @@ export class Quiz extends window.HTMLElement {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
-    this._questionForm = this.shadowRoot.querySelector('#question')
-    this._questionForm.hidden = true
-    this._nicknameForm = this.shadowRoot.querySelector('#nickname')
-    // this._questionTitle = this._questionForm.getFirstElementChild.textContent = ''
-    this._config = {
-      questionID: 1,
-      questionURL: 'http://vhost3.lnu.se:20080/question',
-      answerUrl: 'http://vhost3.lnu.se:20080/answer'
-    }
-    this._nickname = ''
+    this.questionForm = this.shadowRoot.querySelector('#question')
+    this.questionID = 1
   }
 
   connectedCallback () {
-    // check here if alternatives exists in the question.
-    let currentQuestion = this._getQuestion(this._config.questionID)
-    this._addQuestion(currentQuestion)
+    this.getQuestion(this.questionID)
+    this.questionForm.addEventListener('submit', this.submitAnswer)
+  }
 
-    this._questionForm.addEventListener('load', () => {
-      this._questionForm.style.visibility = 'hidden'
-    })
-
-    this._nicknameForm.addEventListener('submit', async event => {
-      event.preventDefault() // removing /q=blabla in the url
-      this.nickname = event.target.nickname.value
-      event.target.hidden = true
-      this._questionForm.hidden = false
-    })
-    
-    this._questionForm.addEventListener('submit', async event => {
-      event.preventDefault()
-      this._sendAnswer(this._config.questionID, event.target.answer.value)
-      // this._questionForm.firstElementChild.textContent = ''
+  getQuestion (id) {
+    window.fetch(`http://vhost3.lnu.se:20080/question/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      this.questionForm.firstElementChild.textContent = data.question 
     })
   }
 
-  _addQuestion (question) {
-    let questionForm = this._questionForm
-    question.then(function (q) {
-      // Changing the question title
-      questionForm.firstElementChild.textContent = q.question
-    })
-  }
+  submitAnswer (event) {
+    event.preventDefault()
 
-  async _getQuestion (id) {
-    let questionResult = await window.fetch(`${this._config.questionURL}/${id}`)
-    return questionResult.json()
-  }
+    let answer = event.target.answer.value
+    let id = this.questionID
 
-  async _sendAnswer (id, answer) {
-    let config = this._config
-
-    let postReq = await window.fetch(`${config.answerUrl}/${id}`, {
+    window.fetch(`http://vhost3.lnu.se:20080/answer/${id}`, {
       method: 'POST',
       headers: {
-        'Accept': 'application/json', // ta bort?
+        'Accept': 'application/json, text/plain',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ answer: answer })
-    }).then((response) => {
-      if (response.ok) {
-        response.json().then(function(data) {
-          let nextURL = data.nextURL
-          // we retrieve response object and add the new questionURL and questionID to config object
-          config.questionURL = nextURL
-          config.questionID = nextURL.substring(nextURL.lastIndexOf('/') + 1)
-        })
-      } else {
-        console.error('Something went wrong!')
-      }
-   })
+      body: JSON.stringify( {answer: answer} )
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
   }
 
 }
